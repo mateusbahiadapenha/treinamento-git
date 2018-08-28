187637c396c8636b563298b1393eb0a0ee4d54da
mateusbahiadapenha <mateusbahia375@gmail.com>
2018-08-27 21:24:12 -0300

    auteraçao no xemplo1 e exemplo2

--- exemplo01.php
+++ exemplo01.php
@@ -1,18 +1,17 @@
 <?php 
 
 class pessoa{
-	
+	   //protected $altura;
+	   protected $nome;
+	 public function setNome($nome){
+	 	$this->nome = $nome ;
+	 }
+	 public function  andar(){
+	 	echo "andar";
+	 }
 	
 }
 
-class cadeira{
-	
-	
-}
 
-
-class caneta{
-	
-}
 
  ?>
--- exemplo02.php
+++ exemplo02.php
@@ -1,7 +1,12 @@
 <?php 
 
 require_once'exemplo01.php';
+$pessoa = new pessoa;
+$pessoa->setNome("mateus penha");
+$pessoa->andar()
+var_dump($pessoa);
 
-$c1= new pessoa();
 
+
+ 
  ?>
